e40d4b8dc9fdcc498c8e301d0dd426fd9bd401f8
leyna1118 <b08902057@ntu.edu.tw>
2023-04-05 01:14:21 +0800

    drag touch

--- input.js
+++ input.js
@@ -8,6 +8,8 @@ of the interaction.
 */
 
 let lastClick = null;
+let curTouch = null;
+let workspaceTouchstarted = false;
 let wasDragging = false;
 let dragging = false;
 let curDrag = null;
@@ -16,128 +18,142 @@ const workspace = document.querySelector('#workspace');
 const targets = document.querySelectorAll('.target');
 
 // click
-workspace.addEventListener('click', function(e){
-    console.log('workspace click');
-    // put target back of a target and click
-    if(wasDragging == true){
-        wasDragging = false;
-        return;
-    }
-    if(lastClick != null)
-        lastClick.style.backgroundColor = '#f00';
-});
+// workspace.addEventListener('click', function(e){
+//     console.log('workspace click');
+//     // put target back of a target and click
+//     if(wasDragging == true){
+//         wasDragging = false;
+//         return;
+//     }
+//     if(lastClick != null)
+//         lastClick.style.backgroundColor = '#f00';
+// });
 
-document.addEventListener('keydown', function(e){
-    if(e.key === 'Escape'){
-        console.log('esc');
-        if(dragging){
-            curDrag.style.left = lastState.l + 'px';
-            curDrag.style.top = lastState.t + 'px';
-            dragging = false;
-            curDrag = null;
-        }
-    }
-});
+// document.addEventListener('keydown', function(e){
+//     if(e.key === 'Escape'){
+//         console.log('esc');
+//         if(dragging){
+//             curDrag.style.left = lastState.l + 'px';
+//             curDrag.style.top = lastState.t + 'px';
+//             dragging = false;
+//             curDrag = null;
+//         }
+//     }
+// });
 
-for(let i = 0; i < targets.length; i++){
-    // blue
-    targets[i].addEventListener('click', function(e){
-        console.log('target click');
-        if(wasDragging == true){
-            wasDragging = false;
-            e.stopPropagation();
-            return;
-        }
-        if(lastClick != null)
-            lastClick.style.backgroundColor = '#f00';
-        this.style.backgroundColor = '#00f';
-        lastClick = this;
-        e.stopPropagation();
-    });
+// for(let i = 0; i < targets.length; i++){
+//     // blue
+//     targets[i].addEventListener('click', function(e){
+//         console.log('target click');
+//         if(wasDragging == true){
+//             wasDragging = false;
+//             e.stopPropagation();
+//             return;
+//         }
+//         if(lastClick != null)
+//             lastClick.style.backgroundColor = '#f00';
+//         this.style.backgroundColor = '#00f';
+//         lastClick = this;
+//         e.stopPropagation();
+//     });
     
-    // drag
-    targets[i].addEventListener('mousedown', function(e){
-        console.log('mousedown');
-        dragging = true;
-        curDrag = this;
-        lastState.l = this.offsetLeft;
-        lastState.t = this.offsetTop;
-        e.stopPropagation();
-    });
+//     // drag
+//     targets[i].addEventListener('mousedown', function(e){
+//         console.log('mousedown');
+//         dragging = true;
+//         curDrag = this;
+//         lastState.l = this.offsetLeft;
+//         lastState.t = this.offsetTop;
+//         e.stopPropagation();
+//     });
 
-    document.addEventListener('mousemove', function(e){
-        if(dragging){
-            console.log('mousemove');
-            curDrag.style.left = e.pageX - curDrag.offsetWidth / 2 + 'px';
-            curDrag.style.top = e.pageY - curDrag.offsetHeight / 2 + 'px';
-            wasDragging = true;
-        }
-    });
+//     document.addEventListener('mousemove', function(e){
+//         if(dragging){
+//             console.log('mousemove');
+//             curDrag.style.left = e.pageX - curDrag.offsetWidth / 2 + 'px';
+//             curDrag.style.top = e.pageY - curDrag.offsetHeight / 2 + 'px';
+//             wasDragging = true;
+//         }
+//     });
 
-    document.addEventListener('mouseup', function(e){
-        console.log('mouseup');
-        dragging = false;
-        curDrag = null;
-    });
+//     document.addEventListener('mouseup', function(e){
+//         console.log('mouseup');
+//         dragging = false;
+//         curDrag = null;
+//     });
 
-    //double
-    targets[i].addEventListener('dblclick', function(e){
-        console.log('dbclick');
-        dragging = true;
-        curDrag = this;
-        e.stopPropagation();
-    });
-}
+//     //double
+//     targets[i].addEventListener('dblclick', function(e){
+//         console.log('dbclick');
+//         dragging = true;
+//         curDrag = this;
+//         e.stopPropagation();
+//     });
+// }
 
 
 // touch
+workspace.addEventListener('touchstart', function(e){
+    console.log('workspace touchstart');
+    e.preventDefault();
+    workspaceTouchstarted = true;
+});
+
 for(let i = 0; i < targets.length; i++){
-    // blue
+    // drag
     targets[i].addEventListener('touchstart', function(e){
         console.log('target touchstart');
         e.preventDefault();
-        if(wasDragging == true){
-            wasDragging = false;
-            e.stopPropagation();
-            return;
+        curTouch = this;
+
+        touchIdentifier = e.targetTouches[0].identifier;
+        dragging = true;
+        curDrag = this;
+        lastState.l = parseInt(this.style.left) || 0;;
+        lastState.t = parseInt(this.style.top) || 0;;
+        e.stopPropagation();
+    });
+
+}
+
+document.addEventListener('touchmove', function(e){
+    // find the touch that started the drag
+    let touch = null;
+    for (let i = 0; i < e.changedTouches.length; i++) {
+        if (e.changedTouches[i].identifier === touchIdentifier) {
+            touch = e.changedTouches[i];
+            break;
         }
+    }
+    if (touch && dragging) {
+        console.log('touchmove');
+        curDrag.style.left = touch.pageX - curDrag.offsetWidth / 2 + 'px';
+        curDrag.style.top = touch.pageY - curDrag.offsetHeight / 2 + 'px';
+        wasDragging = true;
+    }
+});
+
+document.addEventListener('touchend', function(e){
+    if(workspaceTouchstarted){
+        console.log('workspace touchend');
         if(lastClick != null)
             lastClick.style.backgroundColor = '#f00';
-        this.style.backgroundColor = '#00f';
-        lastClick = this;
-        e.stopPropagation();
-    });
-    
-    // drag
-    targets[i].addEventListener('mousedown', function(e){
-        console.log('mousedown');
-        dragging = true;
-        curDrag = this;
-        lastState.l = this.offsetLeft;
-        lastState.t = this.offsetTop;
-        e.stopPropagation();
-    });
+        workspaceTouchstarted = false;
+        return;
+    }
 
-    document.addEventListener('mousemove', function(e){
-        if(dragging){
-            console.log('mousemove');
-            curDrag.style.left = e.pageX - curDrag.offsetWidth / 2 + 'px';
-            curDrag.style.top = e.pageY - curDrag.offsetHeight / 2 + 'px';
-            wasDragging = true;
-        }
-    });
-
-    document.addEventListener('mouseup', function(e){
-        console.log('mouseup');
+    if (e.changedTouches[0].identifier === touchIdentifier) {
+        console.log('target touchend');
+        touchIdentifier = null;
         dragging = false;
         curDrag = null;
-    });
+    }
 
-    //double
-    targets[i].addEventListener('dblclick', function(e){
-        console.log('dbclick');
-        dragging = true;
-        curDrag = this;
-        e.stopPropagation();
-    });
-}
+    if(!wasDragging){
+        if(lastClick != null)
+            lastClick.style.backgroundColor = '#f00';
+        curTouch.style.backgroundColor = '#00f';
+        wasDragging = false;
+        lastClick = curTouch;
+    }
+});
