090acc0507218cca20772ccb7515dbe474053347
leyna1118 <b08902057@ntu.edu.tw>
2023-04-04 22:15:04 +0800

    blue and unblue

--- input.js
+++ input.js
@@ -6,3 +6,24 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
+let lastBlue = null;
+
+const workspace = document.querySelector('#workspace');
+workspace.addEventListener('click', unBlue);
+function unBlue(){
+    if(lastBlue != null)
+        lastBlue.style.backgroundColor = '#f00';
+}
+
+const targets = document.querySelectorAll('.target');
+for(let i = 0; i < targets.length; i++){
+    targets[i].addEventListener('click', blue);
+}
+
+function blue(e){
+    if(lastBlue != null)
+        lastBlue.style.backgroundColor = '#f00';
+    this.style.backgroundColor = '#00f';
+    lastBlue = this;
+    e.stopPropagation();
+}
