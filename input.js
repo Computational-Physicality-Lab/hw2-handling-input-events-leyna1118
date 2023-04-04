606f791beb3ec2ae79c9373451c91c9e81addcfd
leyna1118 <b08902057@ntu.edu.tw>
2023-04-05 00:17:39 +0800

    blue touch

--- input.js
+++ input.js
@@ -12,8 +12,10 @@ let wasDragging = false;
 let dragging = false;
 let curDrag = null;
 let lastState = {l: 0, t: 0, h: 0, w: 0};
-
 const workspace = document.querySelector('#workspace');
+const targets = document.querySelectorAll('.target');
+
+// click
 workspace.addEventListener('click', function(e){
     console.log('workspace click');
     // put target back of a target and click
@@ -37,11 +39,63 @@ document.addEventListener('keydown', function(e){
     }
 });
 
-const targets = document.querySelectorAll('.target');
 for(let i = 0; i < targets.length; i++){
     // blue
     targets[i].addEventListener('click', function(e){
         console.log('target click');
+        if(wasDragging == true){
+            wasDragging = false;
+            e.stopPropagation();
+            return;
+        }
+        if(lastClick != null)
+            lastClick.style.backgroundColor = '#f00';
+        this.style.backgroundColor = '#00f';
+        lastClick = this;
+        e.stopPropagation();
+    });
+    
+    // drag
+    targets[i].addEventListener('mousedown', function(e){
+        console.log('mousedown');
+        dragging = true;
+        curDrag = this;
+        lastState.l = this.offsetLeft;
+        lastState.t = this.offsetTop;
+        e.stopPropagation();
+    });
+
+    document.addEventListener('mousemove', function(e){
+        if(dragging){
+            console.log('mousemove');
+            curDrag.style.left = e.pageX - curDrag.offsetWidth / 2 + 'px';
+            curDrag.style.top = e.pageY - curDrag.offsetHeight / 2 + 'px';
+            wasDragging = true;
+        }
+    });
+
+    document.addEventListener('mouseup', function(e){
+        console.log('mouseup');
+        dragging = false;
+        curDrag = null;
+    });
+
+    //double
+    targets[i].addEventListener('dblclick', function(e){
+        console.log('dbclick');
+        dragging = true;
+        curDrag = this;
+        e.stopPropagation();
+    });
+}
+
+
+// touch
+for(let i = 0; i < targets.length; i++){
+    // blue
+    targets[i].addEventListener('touchstart', function(e){
+        console.log('target touchstart');
+        e.preventDefault();
         if(wasDragging == true){
             wasDragging = false;
             e.stopPropagation();
